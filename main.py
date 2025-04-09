1302c03f285a596162856124b1af1ea28a850da7
Jesuslalo <jesuslalopelo@gmail.com>
2025-04-08 19:33:26 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index02')
+def index02():
+    return render_template('index02.html')
+
+@app.route('/index03')
+def index03():
+    return render_template('index03.html')
+
+@app.route('/index04')
+def index04():
+    return render_template('index04.html')
+
+@app.route('/index05')
+def index05():
+    return render_template('index05.html')
+
+@app.route('/index06')
+def index06():
+     return render_template('index06.html')
+if __name__ == '__main__':
+    app.run(debug=True)
